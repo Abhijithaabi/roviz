2797c636a58d1db6c3242ee6b928973a3c856432
abhijithaabi <abhijithaabi@gmail.com>
2019-09-02 21:24:45 +0530

    python_script

--- catkin_ws/build/chefbot_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/abi/catkin_ws/src/chefbot_description
-# Build directory: /home/abi/catkin_ws/build/chefbot_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- catkin_ws/build/chefbot_gazebo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/abi/catkin_ws/src/chefbot_gazebo
-# Build directory: /home/abi/catkin_ws/build/chefbot_gazebo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
